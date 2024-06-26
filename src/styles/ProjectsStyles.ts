51720d0c4d590d132ecbfc4d07d4884b40737639
Avdika <avdika@ukr.net>
2024-06-26 23:00:11 +0200

    update Project page

---
+++ src/styles/ProjectsStyles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ProjectsContainer = styled.div`
+  display: flex;
+`;
+
+export const Content = styled.div`
+  flex-grow: 1;
+  padding: 20px;
+`;
