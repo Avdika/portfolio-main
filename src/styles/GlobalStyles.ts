097e787122cbaaed4495172ade6fb8414936ab31
Avdika <avdika@ukr.net>
2024-06-26 22:25:30 +0200

    add draft styles for mobile

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -7,6 +7,18 @@ const GlobalStyle = createGlobalStyle<{ theme: Theme }>`
     padding: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
   }
+
+  @media (max-width: 768px) {
+    body {
+      font-size: 14px;
+    }
+  }
+
+  @media (max-width: 480px) {
+    body {
+      font-size: 12px;
+    }
+  }
 `;
 
 export default GlobalStyle;
